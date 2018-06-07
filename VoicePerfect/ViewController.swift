be5d1d3fb3da0d8a988ee53d33f8cfa5bddd3bf7
elmar <elmar.bayer@gmail.com>
2018-06-07 13:45:01 +0200

    First commit

---
+++ VoicePerfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoicePerfect
+//
+//  Created by ELMAR BAYER on 07.06.18.
+//  Copyright © 2018 ELMAR BAYER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
