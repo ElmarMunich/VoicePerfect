eeb5d6492f9c7519c96424e24466a9adb6b6e560
elmar <elmar.bayer@gmail.com>
2018-06-09 16:49:59 +0200

    Layout almost fixed

--- VoicePerfect/PlayViewController.swift
+++ VoicePerfect/PlayViewController.swift
@@ -10,29 +10,39 @@ import UIKit
 import AVFoundation
 
 class PlayViewController: UIViewController {
+    
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var darkVaderButton: UIButton!
+    @IBOutlet weak var parrotButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var recordNewSoundButton: UIButton!
+    
     // variable for receiving audio from the RecordViewController
     var recordedAudioURL: URL!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    
+    @IBAction func playSoundForButton(_ sender: Any) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func pauseButton(_ sender: Any) {
     }
-    */
+    
+    @IBAction func RecordNewSoundButton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAudio()
+    }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
 }
--- VoicePerfect/RecordViewController.swift
+++ VoicePerfect/RecordViewController.swift
@@ -79,7 +79,6 @@ class RecordViewController: UIViewController, AVAudioRecorderDelegate {
                 playSoundsVC.recordedAudioURL = recordedAudioURL
             }
         }
-
     }
     
 }
